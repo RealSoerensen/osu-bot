06ee7f6bc533134a1290f964be3df3d6bce7f14a
RealSoerensen <patricklykke@live.dk>
2021-11-22 20:16:53 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+import os
+
+
+
+class osu_bot():
+    def __init__(self):
+        self.cap = cv.VideoCapture(0)
+
+
+if __name__ == '__main__':
+    bot = osu_bot()
